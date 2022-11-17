3edf547e71b947cdeccdd3f8c7f68629152d166c
David Zavala <86077556+DaveZarp@users.noreply.github.com>
2022-11-17 12:43:29 -0600

    Create PruebaLed.C

---
+++ PruebaLed.C
@@ -0,0 +1,20 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+
+int main(void)
+{
+	//WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    int k;
+	PM5CTL0 &= ~0x01;
+	P1DIR |= 0X03; // P0 y P1 OUTPUT
+	while(1) {
+	    P1OUT = 0x02;
+
+	    P1OUT = 0x01;
+	}
+	
+}
